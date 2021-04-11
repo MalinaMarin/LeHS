833b393f06b2e079a5acb5894ba2f73b099459ba
RomilaIngridMaria <62561385+RomilaIngridMaria@users.noreply.github.com>
2021-04-11 22:15:54 +0300

    hint problem sollved

--- Navbar&footer/script.js
+++ Navbar&footer/script.js
@@ -2,46 +2,68 @@ const navToggler = document.querySelector(".nav-toggler");
 navToggler.addEventListener("click", navToggle);
 
 function navToggle() {
-   navToggler.classList.toggle("active");
-   const nav = document.querySelector(".nav-right");
-   nav.classList.toggle("open");
-   if(nav.classList.contains("open")){
-       nav.style.maxHeight = nav.scrollHeight + "px";
-   }
-   else{
-       nav.removeAttribute("style");
-   }
-} 
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".nav-right");
+    nav.classList.toggle("open");
+    if (nav.classList.contains("open")) {
+        nav.style.maxHeight = nav.scrollHeight + "px";
+    }
+    else {
+        nav.removeAttribute("style");
+    }
+}
 
 const submenuToggler = document.querySelector(".nav-toggler-submenu");
 submenuToggler.addEventListener("click", submenuToggle);
 
 function submenuToggle() {
-   submenuToggler.classList.toggle("active");
-   const submenu = document.querySelector(".sub-menu");
-   submenu.classList.toggle("open");
-   if(submenu.classList.contains("open")){
-       submenu.style.maxHeight = submenu.scrollHeight + "px";
-   }
-   else{
-         submenu.removeAttribute("style");
-   }
-} 
+    submenuToggler.classList.toggle("active");
+    const submenu = document.querySelector(".sub-menu");
+    submenu.classList.toggle("open");
+    if (submenu.classList.contains("open")) {
+        submenu.style.maxHeight = submenu.scrollHeight + "px";
+    }
+    else {
+        submenu.removeAttribute("style");
+    }
+}
 
 
 
 
 
-var menu=document.getElementById("menu-btn");
-var xsubmenu=document.getElementById("submenu");
-menu.addEventListener("mouseover", function (){const submenu=document.getElementById("submenu");
-submenu.style.display="block";
-xsubmenu.addEventListener("mouseover", function (){var submenu=document.getElementById("submenu");
-submenu.style.display="block";});
-xsubmenu.addEventListener("mouseleave", function (){var submenu=document.getElementById("submenu");
-submenu.style.display="none";});
-menu.addEventListener("mouseleave", function (){var submenu=document.getElementById("submenu");
-submenu.style.display="none";
+// var menu=document.getElementById("menu-btn");
+// var xsubmenu=document.getElementById("submenu");
+// menu.addEventListener("mouseover", function (){const submenu=document.getElementById("submenu");
+// submenu.style.display="block";
+// xsubmenu.addEventListener("mouseover", function (){var submenu=document.getElementById("submenu");
+// submenu.style.display="block";});
+// xsubmenu.addEventListener("mouseleave", function (){var submenu=document.getElementById("submenu");
+// submenu.style.display="none";});
+// menu.addEventListener("mouseleave", function (){var submenu=document.getElementById("submenu");
+// submenu.style.display="none";
+// });
+// });
+
+var menu = document.getElementById("menu-btn");
+var xsubmenu = document.getElementById("submenu");
+menu.addEventListener("mouseover", function () {
+    const submenu = document.getElementById("submenu");
+    submenu.style.display = "block";
+    document.getElementById("hint").style.display="none";
+    xsubmenu.addEventListener("mouseover", function () {
+        var submenu = document.getElementById("submenu");
+        submenu.style.display = "block";
+        document.getElementById("hint").style.display="none";
+    });
+    xsubmenu.addEventListener("mouseleave", function () {
+        var submenu = document.getElementById("submenu");
+        submenu.style.display = "none";
+        document.getElementById("hint").style.display="block";
+    });
+    menu.addEventListener("mouseleave", function () {
+        var submenu = document.getElementById("submenu");
+        submenu.style.display = "none";
+        document.getElementById("hint").style.display="block";
+    });
 });
-});
-
--- Play-page/play.js
+++ Play-page/play.js
@@ -2,3 +2,7 @@ var hint=document.getElementById("hint");
 hint.addEventListener("click", function (){var texthint=document.getElementById("text-hint");
 texthint.style.visibility="visible";
 });
+
+
+var sub=document.getElementById("submit");
+sub.addEventListener("click",function(){location.href = "../Level-Map/map.html";})
