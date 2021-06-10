833b405ce7b8dce49002f556f624994510c8bb66
Raluca Macovei <ralumacovei@yahoo.com>
2021-06-10 03:08:52 +0300

    play levels

--- views/Play-page/play.js
+++ views/Play-page/play.js
@@ -6,3 +6,20 @@ texthint.style.visibility="visible";
 
 var sub=document.getElementById("submit");
 sub.addEventListener("click",function(){location.href = "../Level-Map/map.html";})
+
+
+var run=document.getElementById("run");
+// var iframe_page=document.getElementsByTagName("try.html");
+
+
+run.addEventListener("click",function(){
+    
+    request = new XMLHttpRequest();
+    var code=document.getElementById("code").innerHTML;
+    iframedoc = document.getElementById("my_iframe").contentDocument;
+    iframedoc.getElementById("code").innerHTML="";
+    iframedoc.getElementById("code").innerHTML+=document.getElementById("start").textContent;
+    iframedoc.getElementById("code").innerHTML+=document.getElementById("code").value;
+    iframedoc.getElementById("code").innerHTML+=document.getElementById("end").textContent;
+    
+});
