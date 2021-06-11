957819a2641045f2de22d49154266391214bcedf
MalinaMarin <malinamarin22@gmail.com>
2021-06-11 07:46:16 +0300

    report

--- views/authentification-pages/gitsignup.js
+++ views/authentification-pages/gitsignup.js
@@ -18,8 +18,8 @@ function status(response)
             console.log(response);
             return Promise.reject(new Error ("ERROR.."))
         }
-        else
-        return Promise.reject(new Error("Unexpected error"));
+        //else
+        //return Promise.reject(new Error("Unexpected error"));
 }
 
 function goToPage() {
@@ -51,37 +51,31 @@ function goToPage() {
     }
 }
 
-async function gitsignup() {
-   
+async function gitsignup(e) {
+    e.preventDefault();
+
     //var username = document.getElementById("username").value;
     //var password = document.getElementById("password").value;
     //console.log(username);
     //console.log(password);
+    
+   await fetch('http://localhost:5000/login/github',  {
+     'mode': 'no-cors',
+     // "method" : 'POST'
+    })
+    .then(status)
+    .then((res) => res.json())
+    .then((response) => {console.log(response);
+        localStorage.setItem("user_id", response.user_data.id);
+        localStorage.setItem("username", response.user_data.username);
+        localStorage.setItem("user_xp", response.user_data.xp);
+        localStorage.setItem("user_coins", response.user_data.coins);
+        localStorage.setItem("user_level", response.user_data.current_level);
+        localStorage.setItem("user_practice", response.user_data.practice_questions_solved);
+        localStorage.setItem("access_token", response.access_token);
 
-    const result = await (await fetch('http://localhost:4000/login/github', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })).json();
-
-//    await fetch('http://localhost:5000/login/github', {
-//       "method" : 'POST'
-//     })
-//     .then(status)
-//     .then((res) => res.json())
-//     .then((response) => {console.log(response);
-//         localStorage.setItem("user_id", response.user_data.id);
-//         localStorage.setItem("username", response.user_data.username);
-//         localStorage.setItem("user_xp", response.user_data.xp);
-//         localStorage.setItem("user_coins", response.user_data.coins);
-//         localStorage.setItem("user_level", response.user_data.current_level);
-//         localStorage.setItem("user_practice", response.user_data.practice_questions_solved);
-//         localStorage.setItem("access_token", response.access_token);
-
-//         goToPage();
-//     })
-//     .catch((err) => alert(err))
+        goToPage();
+    })
+    .catch((err) => alert(err))
 
     }
