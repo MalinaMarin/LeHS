7ca1371bbf44fa38560e0a6aaa58982cda38effe
Raluca Macovei <ralumacovei@yahoo.com>
2021-06-11 09:12:00 +0300

    Update user.js

--- views/user.js
+++ views/user.js
@@ -1,14 +1,8 @@
-var username = localStorage.getItem("username");
-var coins = localStorage.getItem("user_coins");
-var xp = localStorage.getItem("user_xp");
-function user_populate() {
-    
-    var name = document.getElementById("menu_btn")
-    var c = document.getElementById("coins");
-    var e = document.getElementById("exp");
 
-    name.textContent.innerHTML = username;
-    c.textContent.innerHTML = coins + " coins";
-    e.textContent.innerHTML = xp + " xp";
+function user_populate() {
+    console.log("hello!");
+    document.getElementById("menu_btn").innerHTML = localStorage.getItem("username");
+    document.getElementById("coins").innerHTML = localStorage.getItem("user_coins");
+    document.getElementById("exp").innerHTML =localStorage.getItem("user_xp");
 
 };
